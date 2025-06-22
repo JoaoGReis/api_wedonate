f2ec4d70e63ddeaeb9a9d6bc53678bb25e8dd07c
Kevin-Melo05 <kevinrezende200@gmail.com>
2025-06-22 16:18:34 -0300

    fix - API Render

--- src/config/database.js
+++ src/config/database.js
@@ -3,31 +3,27 @@ require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-let connectionConfig;
+let pool;
 
 if (isProduction) {
-
     if (!process.env.DATABASE_URL) {
         throw new Error('ERRO CRÍTICO: A variável DATABASE_URL não foi encontrada no ambiente de produção.');
     }
-
-    connectionConfig = {
-        connectionString: `${process.env.DATABASE_URL}?ssl=true`,
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false
         }
-    };
+    });
 } else {
-    connectionConfig = {
+    pool = new Pool({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
-    };
+    });
 }
-
-const pool = new Pool(connectionConfig);
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
